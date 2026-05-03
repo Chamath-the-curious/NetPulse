d65fa4f4825e1774c86a2639c138e906cacba8d5
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-03 23:45:48 +0530

    add logging for scheduled task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,21 +3,25 @@ import logic.NetworkReader;
 import logic.RecordReader;
 import logic.UsageRecorder;
 
-import java.time.LocalDate;
 import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class App {
+
+    private static final Logger logger = Logger.getLogger(UsageRecorder.class.getName());
+
     public static void main(String[] args) {
 
         ScheduledExecutorService scheduler =
                 Executors.newScheduledThreadPool(1);
 
         Runnable task = () -> {
-            System.out.println("Running task at: " + java.time.LocalDateTime.now());
+            logger.log(Level.INFO, "Running task at: " + java.time.LocalDate.now());
 
             Optional<DailyStat> statOptional = NetworkReader.readPerAdapter();
             statOptional.ifPresent(UsageRecorder::record);
