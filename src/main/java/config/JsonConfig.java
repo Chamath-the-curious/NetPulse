c62ef850bdea6c486a82d782c09ef396c03e0c68
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-03 19:11:43 +0530

    add JsonConfig

---
+++ src/main/java/config/JsonConfig.java
@@ -0,0 +1,17 @@
+package config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonConfig {
+
+    public static final ObjectMapper MAPPER = create();
+
+    private static ObjectMapper create() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
