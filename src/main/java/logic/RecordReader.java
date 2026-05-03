90f62362ce8a4141b7ff06ff21443e87e6ab939d
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-03 23:16:35 +0530

    add RecordReader

---
+++ src/main/java/logic/RecordReader.java
@@ -0,0 +1,41 @@
+package logic;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.DailyStat;
+import util.JsonConfig;
+import util.Paths;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RecordReader {
+
+    private static ObjectMapper mapper = JsonConfig.MAPPER;
+    private static final Logger logger = Logger.getLogger(UsageRecorder.class.getName());
+
+    public static Optional<DailyStat> read(LocalDate date) {
+        File file = new File(Paths.DATA_DIR, date + ".json");
+
+        try {
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            return Optional.of(mapper.readValue(file, DailyStat.class));
+
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "File not found for date: " + date);
+            return Optional.empty();
+
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "IO Exception: ", e);
+            return Optional.empty();
+
+        }
+    }
+}
