4b8065ddb62a23c84246f5a3b77d65d3c0b0b773
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-03 21:06:47 +0530

    add a ScheduledExecutorService to run data usage monitoring for every hour

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,24 @@ import domain.DailyStat;
 import logic.NetworkReader;
 import logic.UsageRecorder;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
 public class App {
     public static void main(String[] args) {
-        DailyStat stat = NetworkReader.readPerAdapter();
-        System.out.println(stat);
-        UsageRecorder.record(stat);
+
+        ScheduledExecutorService scheduer =
+                Executors.newScheduledThreadPool(1);
+
+        Runnable task = () -> {
+            System.out.println("Running task at: " + java.time.LocalDateTime.now());
+
+            DailyStat stat = NetworkReader.readPerAdapter();
+            UsageRecorder.record(stat);
+        };
+
+        scheduer.scheduleAtFixedRate(task, 0, 1, TimeUnit.HOURS);
     }
 }
