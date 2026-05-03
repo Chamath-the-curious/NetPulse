5d3231044ea107398489125a04606cc87958a046
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-03 18:14:09 +0530

    add mapper class for daily record

---
+++ src/main/java/core/DailyStat.java
@@ -0,0 +1,22 @@
+package core;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class DailyStat {
+    private LocalDate date;
+    private List<AdapterStat> adapterStats;
+
+    public DailyStat(List<AdapterStat> adapterStats) {
+        date = LocalDate.now();
+        this.adapterStats = adapterStats;
+    }
+
+    public List<AdapterStat> getAdapterStats() {
+        return adapterStats;
+    }
+
+    public void setAdapterStats(List<AdapterStat> adapterStats) {
+        this.adapterStats = adapterStats;
+    }
+}
