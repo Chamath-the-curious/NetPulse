69a2fdf454521cc1858ce8ca6c3ba6a0f80e7cf7
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-03 19:10:46 +0530

    test UsageRecorder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
 import domain.DailyStat;
 import logic.NetworkReader;
+import logic.UsageRecorder;
 
 public class App {
     public static void main(String[] args) {
         DailyStat stat = NetworkReader.readPerAdapter();
         System.out.println(stat);
+        UsageRecorder.record(stat);
     }
 }
