126b2a57ca6615bb2c9024c1de9eb018dcf11072
Chamath Sasmitha <sasmithachamath@gmail.com>
2026-05-04 11:47:20 +0530

    removed ScheduledExecutorService added exception handling to .execute method call

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,33 +1,32 @@
-import domain.DailyStat;
-import logic.DataHandler;
-import util.NetworkReader;
-import util.UsageRecorder;
 
-import java.util.Optional;
+import cli.NetPulseCommand;
+import logic.DataHandler;
+import picocli.CommandLine;
+
+import java.time.LocalDate;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-public class App {
+public class App  {
 
     private static final Logger logger = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
 
-        ScheduledExecutorService scheduler =
-                Executors.newScheduledThreadPool(1);
-
         DataHandler dataHandler = new DataHandler();
 
-        Runnable task = () -> {
-            logger.log(Level.INFO, "Running task at: " + java.time.LocalDate.now());
+        int exitCode = new CommandLine(new NetPulseCommand()).execute(args);
 
+        try {
+            logger.log(Level.INFO, "Running task at: " + LocalDate.now());
             dataHandler.execute();
-        };
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Task failed", e);
+        }
 
-        scheduler.scheduleAtFixedRate(task, 0, 1, TimeUnit.HOURS);
+        System.exit(exitCode);
     }
 }
